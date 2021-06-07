89493169b4096aa1b9b8dac91e64ed54ffc76d0a
Shunji Zhan <shunhahaha@live.com>
2021-06-07 21:16:04 +0800

    set up travis and codedev pipeline

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   // The directory where Jest should output its coverage files
-  // coverageDirectory: 'coverage',
+  coverageDirectory: './coverage/',
+  collectCoverage: true,
 
   // An array of regexp pattern strings used to skip coverage collection
   coveragePathIgnorePatterns: [
@@ -13,18 +14,18 @@ module.exports = {
   // An object that configures minimum threshold enforcement for coverage results
   coverageThreshold: {
     global: {
-      branches: 10,
-      functions: 10,
-      lines: 10,
-      statements: 10,
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
     },
   },
 
   // A list of reporter names that Jest uses when writing coverage reports
-  coverageReporters: [
-    'text',
-    // "lcov",
-  ],
+  // coverageReporters: [
+  // 'text',
+  // "lcov",
+  // ],
 
   // A set of global variables that need to be available in all test environments
   globals: {},
